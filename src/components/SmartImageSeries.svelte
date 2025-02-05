71b95b8edefba0e9e57084426b1b83f0572f43c1
RaymondWHZ <wuhaozhen1106@gmail.com>
2025-02-05 14:13:18 -0600

    feat: load images only after visible

--- src/components/SmartImageSeries.svelte
+++ src/components/SmartImageSeries.svelte
@@ -1,34 +1,48 @@
 <script lang="ts">
+  	import { onDestroy, onMount } from "svelte";
 	import type { Action } from "svelte/action";
 
 	export let additionalClasses: string = "";
 	export let image: string;
 	export let fullWidth: number | undefined = undefined;
-	export let forceDelayLoading: boolean = true;
 
 	$: fullSrc = fullWidth ? image + `&width=${fullWidth}` : image;
+
+	let imageSrc = "";
+	let imageDiv: HTMLDivElement;
+
+	let observer: IntersectionObserver;
+
+	onMount(() => {
+		observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setTimeout(() => {
+						imageSrc = fullSrc;
+					}, 50);
+					observer.disconnect();
+				}
+			});
+		});
+		observer.observe(imageDiv);
+	});
+
+	onDestroy(() => {
+		observer?.disconnect();
+	});
 
 	let loaded = false;
 
 	const onload: Action<HTMLImageElement> = (el) => {
-		if (!forceDelayLoading && el.complete) {
+		el.addEventListener('load', () => {
 			loaded = true;
-		} else {
-			el.addEventListener('load', () => {
-				loaded = true;
-			});
-		}
-		if (forceDelayLoading) {
-			setTimeout(() => {
-				el.src = fullSrc;
-			}, 50);
-		}
+		});
     }
 </script>
 
-<div class={"overflow-clip relative " + additionalClasses}>
+<div class={"overflow-clip relative " + additionalClasses} bind:this={imageDiv}>
 	<img src={image + "&width=200"} alt="" class={"w-full blur-lg transition duration-[600] delay-200" + (loaded ? " opacity-0" : "")} />
 	<div class={"absolute inset-0 transition duration-200 ease-in" + (loaded ? "" : " opacity-0")}>
-		<img use:onload src={forceDelayLoading ? "" : fullSrc} alt="" class={"w-full transition duration-[600] delay-200" + (loaded ? "" : " blur-md")} />
+		<img use:onload src={imageSrc} alt="" class={"w-full transition duration-[600] delay-200" + (loaded ? "" : " blur-md")} />
 	</div>
 </div>
