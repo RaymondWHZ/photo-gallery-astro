8b5bb5312ce846dc27c538023759ed39e272cc5f
RaymondWHZ <wuhaozhen1106@gmail.com>
2025-02-05 10:16:55 -0600

    feat: does not force delay loading on timeline page

--- src/components/SmartImageSeries.svelte
+++ src/components/SmartImageSeries.svelte
@@ -4,28 +4,31 @@
 	export let additionalClasses: string = "";
 	export let image: string;
 	export let fullWidth: number | undefined = undefined;
+	export let forceDelayLoading: boolean = true;
 
-	let loadedLevel = 0;
+	$: fullSrc = fullWidth ? image + `&width=${fullWidth}` : image;
 
-	const onload: Action<HTMLImageElement, number> = (el, level: number) => {
-        el.addEventListener('load', () => {
-            if (level > loadedLevel) {
-				loadedLevel = level;
-			}
-        });
-		setTimeout(() => {
-			if (fullWidth) {
-				el.src = image + `&width=${fullWidth}`;
-			} else {
-				el.src = image;
-			}
-		}, 50);
+	let loaded = false;
+
+	const onload: Action<HTMLImageElement> = (el) => {
+		if (!forceDelayLoading && el.complete) {
+			loaded = true;
+		} else {
+			el.addEventListener('load', () => {
+				loaded = true;
+			});
+		}
+		if (forceDelayLoading) {
+			setTimeout(() => {
+				el.src = fullSrc;
+			}, 50);
+		}
     }
 </script>
 
 <div class={"overflow-clip relative " + additionalClasses}>
-	<img src={image + "&width=200"} alt="" class={"w-full blur-lg transition duration-[600] delay-200" + (loadedLevel > 0 ? " opacity-0" : "")} />
-	<div class={"absolute inset-0 transition duration-200 ease-in" + (loadedLevel < 1 ? " opacity-0" : "")}>
-		<img use:onload={1} alt="" class={"w-full transition duration-[600] delay-200" + (loadedLevel < 1 ? " blur-md" : "")} />
+	<img src={image + "&width=200"} alt="" class={"w-full blur-lg transition duration-[600] delay-200" + (loaded ? " opacity-0" : "")} />
+	<div class={"absolute inset-0 transition duration-200 ease-in" + (loaded ? "" : " opacity-0")}>
+		<img use:onload src={forceDelayLoading ? "" : fullSrc} alt="" class={"w-full transition duration-[600] delay-200" + (loaded ? "" : " blur-md")} />
 	</div>
 </div>
