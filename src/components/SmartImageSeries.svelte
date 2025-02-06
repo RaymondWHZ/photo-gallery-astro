e0060e03e36b386edfe638a4c5c2bfc9c4d49a50
RaymondWHZ <wuhaozhen1106@gmail.com>
2025-02-06 13:49:50 -0600

    feat: completely remove wait if no force delay

--- src/components/SmartImageSeries.svelte
+++ src/components/SmartImageSeries.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  	import { onMount } from "svelte";
 	import type { Action } from "svelte/action";
 
 	const { additionalClasses = "", image, fullWidth, forceDelay = true } = $props<{
@@ -11,40 +10,35 @@
 
 	const fullSrc = $derived(fullWidth ? image + `&width=${fullWidth}` : image);
 
-	let imageSrc = $state("");
-	let imageDiv: HTMLDivElement;
-
-	onMount(() => {
-		let observer = new IntersectionObserver((entries) => {
-			entries.forEach((entry) => {
-				if (entry.isIntersecting) {
-					if (forceDelay) {
-						setTimeout(() => {
-							imageSrc = fullSrc;
-						}, 50);
-					} else {
-						imageSrc = fullSrc;
-					}
-					observer.disconnect();
-				}
-			});
-		});
-		observer.observe(imageDiv);
-		return () => {
-			observer.disconnect();
-		};
-	});
-
+	let startLoading = $state(false);
 	let loaded = $state(false);
+	
+	const imageSrc = $derived(!forceDelay || startLoading ? fullSrc : "");
 
 	const onload: Action<HTMLImageElement> = (el) => {
-		el.addEventListener('load', () => {
+		if (!forceDelay && el.complete) {
 			loaded = true;
-		});
+		} else {
+			$effect(() => {
+				const loadCallback = () => {
+					loaded = true;
+					el.removeEventListener("load", loadCallback);
+				};
+				el.addEventListener('load', loadCallback);
+				return () => {
+					el.removeEventListener("load", loadCallback);
+				};
+			});
+		}
+		if (forceDelay) {
+			setTimeout(() => {
+				startLoading = true;
+			}, 50);
+		}
     }
 </script>
 
-<div class={"overflow-clip relative " + additionalClasses} bind:this={imageDiv}>
+<div class={"overflow-clip relative " + additionalClasses}>
 	<img src={image + "&width=200"} alt="" class={"w-full blur-sm" + (loaded ? " opacity-0" : "")} />
 	<div class="absolute inset-0">
 		<img use:onload src={imageSrc} alt="" class="w-full" />
